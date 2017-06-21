2f38428fd2cb2b0c3c257c062bf45b19b036e3ed
fsoto1 <f.soto@outlook.com>
2017-06-21 00:55:07 -0400

    Agregado modelo de datos y admin site

--- snu/noticias/admin.py
+++ snu/noticias/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Noticia, Destacada
 
-# Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_categoria')
+
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('titulo','fecha','categoria')
+
+class DestacadaAdmin(admin.ModelAdmin):
+    list_display = ('noticia','fecha_destacada','categoria')
+
+admin.site.register(Categoria)
+admin.site.register(Noticia)
+admin.site.register(Destacada)
--- snu/noticias/models.py
+++ snu/noticias/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+	nombre_categoria = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return self.nombre_categoria
+
+	def __str__(self):
+		return self.nombre_categoria
+
+class Noticia(models.Model):
+	titulo = models.CharField(max_length=50)
+	contenido = models.TextField()
+	imagen = models.ImageField(null=True, blank=True, width_field='ancho_imagen',height_field='alto_imagen')
+	alto_imagen = models.IntegerField(default=0)
+	ancho_imagen = models.IntegerField(default=0)
+	fecha = models.DateField()
+	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		return self.titulo
+
+	def __str__(self):
+		return self.titulo
+
+	class Meta:
+		ordering = ["fecha"]
+
+class Destacada(models.Model):
+	noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE)
+	fecha_destacada = models.DateField()
+
+	def __unicode__(self):
+		return self.fecha_destacada
+
+	def __str__(self):
+		return  self.fecha_destacada 
+
+	class Meta:
+		ordering = ["fecha_destacada"]
