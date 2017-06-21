4cb2847365a59470a0be2e1d62817ea7416a1108
fsoto1 <f.soto@outlook.com>
2017-06-21 19:50:10 -0400

    Agregada funcionalidad detalle de noticia

--- snu/noticias/urls.py
+++ snu/noticias/urls.py
@@ -6,4 +6,5 @@ app_name = 'noticias'
 urlpatterns = [
 	#index
 	url(r'^$', views.index, name='index'),
+	url(r'^detalle/$', views.detalle, name='detalle'),
 ]
--- snu/noticias/views.py
+++ snu/noticias/views.py
@@ -3,8 +3,8 @@ from .models import Noticia
 # Create your views here.
 
 def index(request):
-
 	all_noticia = Noticia.objects.all()[:6]
+	destacada = Noticia.objects.filter(destacada=1)[:1]
 	#all_socios = all_socios.filter()
 	#query = request.GET.get("q")
 	#if query:
@@ -17,4 +17,10 @@ def index(request):
 	
 		
 		#return render(request,'socios/buscar_socios.html', {'all_socios': all_socios} )
-	return render(request, 'noticias/home.html', {'all_noticia': all_noticia})
+	return render(request, 'noticias/home.html', {'all_noticia': all_noticia, 'destacada': destacada})
+
+def detalle(request):
+	query = request.GET.get("id")
+	if query:
+		noticia = Noticia.objects.get(id = query)
+	return render(request, 'noticias/noticia_detalle.html', {'noticia': noticia})
