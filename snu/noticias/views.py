02fc8c35651f9d408268aab9bb498570bd9a02c3
user.name <d.pradosoto@uandresbello.edu>
2017-06-22 20:38:56 -0400

    cambios menores

--- snu/noticias/views.py
+++ snu/noticias/views.py
@@ -5,18 +5,6 @@ from .models import Noticia
 def index(request):
 	all_noticia = Noticia.objects.all()[:6]
 	destacada = Noticia.objects.filter(destacada=1)[:1]
-	#all_socios = all_socios.filter()
-	#query = request.GET.get("q")
-	#if query:
-	#	all_socios = all_socios.filter(
-	##		Q(rut_socios__icontains=query) |
-	#		Q(nom_socios__icontains=query) |
-	#		Q(apel_pat_socios__icontains=query) |
-	#		Q(apel_mat_socios__icontains=query)
-	#		).distinct()
-	
-		
-		#return render(request,'socios/buscar_socios.html', {'all_socios': all_socios} )
 	return render(request, 'noticias/home.html', {'all_noticia': all_noticia, 'destacada': destacada})
 
 def detalle(request):
